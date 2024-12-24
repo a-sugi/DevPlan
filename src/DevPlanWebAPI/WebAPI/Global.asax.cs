3ab608ce0dfd201f93e7ea7fc954d8dc779bc692
akiko.sugiura <akiko.sugiura@system-exe.jp>
2024-12-24 09:08:10 +0900

    WebAPI更新

---
+++ src/DevPlanWebAPI/WebAPI/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using DevPlanWebAPI.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace DevPlanWebAPI
+{
+    /// <summary>
+    /// WebApiApplicationクラス
+    /// </summary>
+    /// <seealso cref="System.Web.HttpApplication" />
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        private Logging Log = new Logging();
+
+        /// <summary>
+        /// Applications the start.
+        /// </summary>
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        /// <summary>
+        /// Handles the Error event of the Application control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+            if (exception == null)
+            {
+                return;
+            }
+            Log.WriteErrorLog(Log.Error,exception.Message, exception);
+        }
+    }
+}
